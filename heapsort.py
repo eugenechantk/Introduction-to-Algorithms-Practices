b3182c9c27b88c80de6375fc0b583fbadf8c7298
Eugene Chan <eugenechan@Eugenes-MacBook-Pro.local>
2020-11-19 13:00:35 +0800

    max-heap implementation for heapsort completed

--- heapsort.py
+++ heapsort.py
@@ -1,8 +1,15 @@
+"""
+Building a max heap: making sure that A[Parent(i)] >= A[i] at every i
+"""
 def build_max_heap(arr):
     heap_size = len(arr)-1
+    # only need to traverse half of the node in the heap; only half of the nodes have children nodes
     for i in range(int(heap_size/2),-1,-1):
         max_heapify(arr,i)
 
+"""
+Max Heapify: making sure that the node at A[i] >= A[Left(i)] and A[Right(i)], Left(i) and Right(i) being A[i]'s two children node
+"""
 def max_heapify(arr,i):
     l = i * 2 + 1
     r = i * 2 + 2
@@ -16,6 +23,7 @@ def max_heapify(arr,i):
         largest = r
 
     if largest != i:
+        # only swap places with the largest value node when finish comparing both left and right child
         arr[i], arr[largest] = arr[largest], arr[i]
         max_heapify(arr,largest)
 
